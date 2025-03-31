bff7a41540c9a73cb553303a50404a24a3c8f8c2
Nabemh <nhanior@gmail.com>
2025-03-31 21:46:24 +0100

    basic transaction

---
+++ practice/send_transaction.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const { ethers } = require("ethers");
+
+const key = process.env.INFURA_ID;
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${key}`);
+
+
+const main = async () => {
+    
+}
+
+main()
