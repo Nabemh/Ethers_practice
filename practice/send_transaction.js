9ed59fe2664ae756e866a1455b3f6049907ae67f
Nabemh <nhanior@gmail.com>
2025-04-01 00:14:34 +0100

    accounts prep

--- practice/send_transaction.js
+++ practice/send_transaction.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const { ethers } = require("ethers");
 
 const key = process.env.INFURA_ID;
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${key}`);
+const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${key}`);
+
+const account1 = '0xa99831Ca21ef7C3F2E0617B6941a2b002bf5BAF6'
+const account2 = '0xa19cB97872EB70ec355F4ea6649AD9ec3f2e1ecd'
+
 
 
 const main = async () => {
