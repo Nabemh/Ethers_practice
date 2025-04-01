35855e21169d5e3978248510eaf9c6d172341bb9
Nabemh <nhanior@gmail.com>
2025-04-01 23:04:47 +0100

    abi

--- practice/write_to_contract.js
+++ practice/write_to_contract.js
@@ -11,6 +11,15 @@ const account2 = '' // Your account address 2
 const privateKey1 = '' // Private key of account 1
 const wallet = new ethers.Wallet(privateKey1, provider)
 
+const ERC20_ABI = [
+    "function balanceOf(address) view returns (uint)",
+    "function transfer(address to, uint amount) returns (bool)",
+]
+
+const address = ''
+
+const contract = new ethers.Contract(address, ERC20_ABI, provider)
+
 const main = async () => {
 
 
