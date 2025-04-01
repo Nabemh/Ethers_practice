0e6e7e2f528bdcf574ddf3c00c5fbbd3f337857c
Nabemh <nhanior@gmail.com>
2025-04-01 23:17:36 +0100

    transfer tokens

--- practice/write_to_contract.js
+++ practice/write_to_contract.js
@@ -14,13 +14,30 @@ const wallet = new ethers.Wallet(privateKey1, provider)
 const ERC20_ABI = [
     "function balanceOf(address) view returns (uint)",
     "function transfer(address to, uint amount) returns (bool)",
-]
+];
 
 const address = '0xa36085F69e2889c224210F603D836748e7dC0088'
-
 const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
 const main = async () => {
+
+    const balance = await contract.balanceOf(account1)
+
+    console.log(`\nReading from ${address}\n`)
+    console.log(`Balance of sender: ${balance}\n`)
+
+    const contractWithWallet =  contract.connect(wallet);
+
+    const tx = await contractWithWallet.transfer(account2, balance);
+    await tx.wait()
+
+    console.log(tx)
+
+    const balanceOfSender = await contract.balanceOf(account1)
+    const balanceOfReciever = await contract.balanceOf(account2)
+
+    console.log(`\nReading from ${balanceOfSender}\n`)
+    console.log(`Balance of sender: ${balanceOfReciever}\n`)
 
 
 }
