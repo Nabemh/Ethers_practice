a476488e4e9c383c80c8dbd772c63e24ca177334
Nabemh <nhanior@gmail.com>
2025-04-01 23:07:55 +0100

    Accounts

--- practice/write_to_contract.js
+++ practice/write_to_contract.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
+const INFURA_ID = process.env.INFURA_ID;
 const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
 
-const account1 = '' // Your account address 1
-const account2 = '' // Your account address 2
+const account1 = '0xa99831Ca21ef7C3F2E0617B6941a2b002bf5BAF6'
+const account2 = '0xa19cB97872EB70ec355F4ea6649AD9ec3f2e1ecd'
 
-const privateKey1 = '' // Private key of account 1
+const privateKey1 = process.env.PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey1, provider)
 
 const ERC20_ABI = [
@@ -16,7 +16,7 @@ const ERC20_ABI = [
     "function transfer(address to, uint amount) returns (bool)",
 ]
 
-const address = ''
+const address = '0xa36085F69e2889c224210F603D836748e7dC0088'
 
 const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
