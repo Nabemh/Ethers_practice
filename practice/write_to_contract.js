081ca971a8206fb25e92bc7bc08dd1d684b405f7
Nabemh <nhanior@gmail.com>
2025-04-01 23:34:52 +0100

    address empty

--- practice/write_to_contract.js
+++ practice/write_to_contract.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 const { ethers } = require("ethers");
 
-const INFURA_ID = process.env.INFURA_ID;
-const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
+const key = process.env.INFURA_ID;
+const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${key}`);
 
 const account1 = process.env.ACCOUNT_1
 const account2 = process.env.ACCOUNT_2
 
-const privateKey1 = process.env.PRIVATE_KEY;
-const wallet = new ethers.Wallet(privateKey1, provider)
+const privatekey1 = process.env.PRIVATE_KEY;
+const wallet = new ethers.Wallet(privatekey1, provider);
 
 const ERC20_ABI = [
     "function balanceOf(address) view returns (uint)",
