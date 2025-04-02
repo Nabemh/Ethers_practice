05e68bb6f0eeac0a79e3b72bf9df7d1f87b7758c
Nabemh <nhanior@gmail.com>
2025-04-02 17:51:04 +0100

    ABI

--- practice/events.js
+++ practice/events.js
@@ -5,4 +5,14 @@ const { ethers } = require("ethers");
 const key = process.env.INFURA_ID;
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${key}`);
 
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+
+    "event Transfer(address indexed from, address indexed to, uint amount)"
+];
+
 const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' //DAI contract
+const contract = new ethers.Contract(address, ERC20_ABI, provider)
