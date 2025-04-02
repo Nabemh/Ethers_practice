7b6fd88023b4385beb6c2182fe062c2a9251c6e3
Nabemh <nhanior@gmail.com>
2025-04-02 17:57:25 +0100

    call main

--- practice/events.js
+++ practice/events.js
@@ -18,8 +18,10 @@ const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' //DAI contract
 const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
 const main = async () => {
-    
+    const block = await provider.getBlockNumber()
 
-    const transferEvents = await contract.queryFilter("Transfer")
+    const transferEvents = await contract.queryFilter("Transfer", block - 10, block)
     console.log(transferEvents)
 }
+
+main()
